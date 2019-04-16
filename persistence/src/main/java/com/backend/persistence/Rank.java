71a60676dc4f5c00f81b0e901e9e97f04434216e
Jorge Atuesta <jatuest@bancodebogota.com.co>
2019-04-16 11:51:03 -0500

    Commit Oreilly

---
+++ persistence/src/main/java/com/backend/persistence/Rank.java
@@ -0,0 +1,5 @@
+package com.backend.persistence;
+
+public enum Rank {
+    ENSIGN, LIEUTENANT, COMMANDER, CAPTAIN, COMMODORE, ADMIRAL
+}
--- restclient/src/test/java/com/backend/restclient/RestclientApplicationTests.java
+++ restclient/src/test/java/com/backend/restclient/RestclientApplicationTests.java
@@ -1,16 +1,36 @@
 package com.backend.restclient;
 
+import com.backend.restclient.services.JokeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestclientApplicationTests {
 
+
+	private Logger logger = LoggerFactory.getLogger(JokeService.class);
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	private JokeService service;
+
+	@Test
+	public void getJoke() throws Exception {
+		String joke = service.getJoke("Craig", "Walls");
+		logger.info(joke);
+		assertTrue(joke.contains("Craig") ||
+				joke.contains("Walls"));
 	}
 
 }
