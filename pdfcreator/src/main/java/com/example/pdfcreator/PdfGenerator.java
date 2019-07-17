8ea5beca0ea2804d824d8fd5cbf1bebedcace2bf
Jorge Atuesta <jatuest@bancodebogota.com.co>
2019-07-17 11:47:21 -0500

    pdf funciona

--- pdfcreator/src/main/java/com/example/pdfcreator/PdfGenerator.java
+++ pdfcreator/src/main/java/com/example/pdfcreator/PdfGenerator.java
@@ -22,11 +22,11 @@ public class PdfGenerator {
 //        createPdf("HelloLetter.pdf", "field", "value");
         pdfGenerator.generatePdfEmployee();
         System.out.println(".............-------·······........");
-        pdfGenerator.generatePdfIndependent();
+       pdfGenerator.generatePdfIndependent();
     }
 
     private void generatePdfEmployee() throws IOException, DocumentException {
-        Resource resource = new ClassPathResource("insurances/Employees.pdf", getClass().getClassLoader());
+        Resource resource = new ClassPathResource("insurances/653 Solicitud de seguro.pdf", getClass().getClassLoader());
         RandomAccessSourceFactory randomAccessSourceFactory = new RandomAccessSourceFactory();
         RandomAccessFileOrArray letter = new RandomAccessFileOrArray(randomAccessSourceFactory.createSource(resource.getInputStream()));
         PdfReader reader = new PdfReader(letter, null);
@@ -34,49 +34,46 @@ public class PdfGenerator {
         PdfStamper stamper = new PdfStamper(reader, baos);
         AcroFields form = stamper.getAcroFields();
         Set<String> fldNames = form.getFields().keySet();
-        form.setField("No de Documento", "1032393128");
-        form.setField("E mail", "correo@correo.com");
-        form.setField("Dirección de Residencia", "Dirección de Residencia");
-        form.setField("No de Identificación", "No de Identificación");
-        form.setField("Text9", "9");
-        form.setField("Text8", "8");
-        form.setField("Text7", "7");
-        form.setField("Text3", "3");
-        form.setField("Text2", "2");
-        form.setField("Text1", "1");
-        form.setField("Text5", "5");
-        form.setField("Text4", "4");
-        form.setField("Text6", "6");
-        form.setField("Consecutivo", "0000001");
-        form.setField("Nombres y Apellidos DATOS DEL ASEGURADO", "Nombres y Apellidos DATOS DEL ASEGURADO");
-        form.setField("Valor del Crédito inicialmente Solicitado", "Valor del Crédito inicialmente Solicitado");
-        form.setField("Lugar de Nacimiento", "Lugar de Nacimiento");
-        form.setField("Código de la Oficina", "Código de la Oficina");
-        form.setField("Teléfono", "Teléfono");
-        form.setField("enfermedad que ha padecido Tipo de enfermedad", "enfermedad que ha padecido Tipo de enfermedad");
-        form.setField("Valor prima Única", "Valor prima Única");
-        form.setField("Ciudad", "Ciudad");
-        form.setField("País de Residencia", "País de Residencia");
-        form.setField("Departamento", "Departamento");
-        form.setField("Nombre del Comercial", "Nombre del Comercial");
-        form.setField("Cédula del Comercial", "Cédula del Comercial");
-        form.setField("Valor Crédito con Seguro", "Valor Crédito con Seguro");
-        form.setField("Nacionalidad", "Nacionalidad");
-        form.setField("Nombre de la oficina", "Nombre de la oficina");
+        form.setField( "BIRTH_DAY2" , "5");
+        form.setField( "NATIONALITY" , "Colombiano");
+        form.setField( "NAMES" , "Jorge Andres Atuesta Maestre");
+        form.setField( "BIRTH_DAY1" , "0");
+        form.setField( "DEPARTAMENT" , "Bogota");
+        form.setField( "INITIAL_CREDIT_AMOUNT" , "2500000");
+        form.setField( "PHONE" , "5647783");
+        form.setField( "RESIDENCE_COUNTRY" , "Colombia");
+        form.setField( "SOL_YEAR" , "2019");
+        form.setField( "BIRTH_MONTH2" , "8");
+        form.setField( "MAIL" , "jatuest@bancodebogota.com.co");
+        form.setField( "SOL_MONTH" , "07");
+        form.setField( "BIRTH_MONTH1" , "0");
+        form.setField( "CONSECUTIVE_NUMBER" , "0000001");
+        form.setField( "OFFICE_NAME" , "OFICINA PRINCIPAL");
+        form.setField( "IDENTIFICATION_NUMBER_SIGN" , "1032393128");
+        form.setField( "SOL_DAY" , "23");
+        form.setField( "SELLER_IDENTIFICATION" , "1098468987");
+        form.setField( "CREDIT_AND_INSURANCE_AMOUNT" , "3000000");
+        form.setField( "BIRTH_YEAR" , "1987");
+        form.setField( "RESIDENCE_ADDRESS" , "Carrera 36 # 17-333 Torre 1 Apartamento 504");
+        form.setField( "BIRTHPLACE" , "Bogota");
+        form.setField( "INSURANCE_AMOUNT" , "500000");
+        form.setField( "CITY" , "Bogota");
+        form.setField( "SELLER_NAME" , "Npmbre del vendedor");
+        form.setField( "IDENTIFICATION_NUMBER" , "1032393128");
+        form.setField( "OFFICE_CODE" , "00000");
 
         for (String fldName : fldNames) {
-            System.out.println("form.setField(\"" + fldName + "\", \"" + fldName + "\");");
+            System.out.println("form.setField( \"" + fldName + "\" , \"" + fldName +"\");");
         }
-     /*   String states[] = form.getAppearanceStates("Check Box11");
-        form.setField("Check Box11", states[0]);  //SEXO FEMENINO
-        form.setField("Check Box10", states[0]);  //SEXO MASCULINO
-        form.setField("Check Box13", states[0]);  //CANAL DE VENTAS UNIDADES
-        form.setField("Check Box12", states[0]); //CANAL DE VENTAS GNV
-        form.setField("Check Box14", states[0]);  //CANAL DE VENTAS OFICINA
-        states = form.getAppearanceStates("IdentificacionPS");
-        form.setField("IdentificacionCC", states[0]);  //CANAL DE VENTAS OFICINA
-        form.setField("IdentificacionCE", states[0]);  //CANAL DE VENTAS OFICINA
-        form.setField("IdentificacionPS", states[0]);  //CANAL DE VENTAS OFICINA */
+       String states[] = form.getAppearanceStates("MALE");
+        form.setField( "IDENTIFICATION_CE" , states[0]);
+        form.setField( "IDENTIFICATION_PS" , states[0]);
+        form.setField( "IDENTIFICATION_CC" , states[0]);
+        form.setField( "FEMALE" , states[0]);
+        form.setField( "MALE" , states[0]);
+        form.setField( "GNV_SALE" , states[0]);
+        form.setField( "UNIT_SALE" , states[0]);
+        form.setField( "OFFICE_SALE" , states[0]);
 
         stamper.setFormFlattening(true);
         stamper.close();
@@ -137,7 +134,7 @@ public class PdfGenerator {
     }
 
     private void generatePdfIndependent() throws IOException, DocumentException {
-        Resource resource = new ClassPathResource("insurances/IndependientesMod.pdf", getClass().getClassLoader());
+        Resource resource = new ClassPathResource("insurances/654 Solicitud de seguro.pdf", getClass().getClassLoader());
         RandomAccessSourceFactory randomAccessSourceFactory = new RandomAccessSourceFactory();
         RandomAccessFileOrArray letter = new RandomAccessFileOrArray(randomAccessSourceFactory.createSource(resource.getInputStream()));
         PdfReader reader = new PdfReader(letter, null);
@@ -145,68 +142,47 @@ public class PdfGenerator {
         PdfStamper stamper = new PdfStamper(reader, baos);
         AcroFields form = stamper.getAcroFields();
         Set<String> fldNames = form.getFields().keySet();
-        form.setField("E mail", "E mail");
-        form.setField("No DOCUMENTO DE IDENTIDADRow1", "No DOCUMENTO DE IDENTIDADRow1");
-        form.setField("No DOCUMENTO DE IDENTIDADRow2", "No DOCUMENTO DE IDENTIDADRow2");
-        form.setField("No DOCUMENTO DE IDENTIDADRow3", "No DOCUMENTO DE IDENTIDADRow3");
-        form.setField("No DOCUMENTO DE IDENTIDADRow4", "No DOCUMENTO DE IDENTIDADRow4");
-        form.setField("Valor prima Única", "Valor prima Única");
-        form.setField("Valor del Crédito inicialmente Solicitado", "Valor del Crédito inicialmente Solicitado");
-        form.setField("Lugar de Nacimiento", "Lugar de Nacimiento");
-        form.setField("APELLIDOSRow1", "APELLIDOSRow1");
-        form.setField("APELLIDOSRow2", "APELLIDOSRow2");
-        form.setField("APELLIDOSRow3", "APELLIDOSRow3");
-        form.setField("Teléfono", "Teléfono");
-        form.setField("APELLIDOSRow4", "APELLIDOSRow4");
-
-        form.setField("Ciudad", "Ciudad");
-
-        form.setField("Text10", "10");
-        form.setField("Nacionalidad", "Nacionalidad");
-        form.setField("Consecutivo", "0000001");
-        form.setField("Nombre de la oficina", "Nombre de la oficina");
-        form.setField(" DE PARTICIPACIÓN Row1", " DE PARTICIPACIÓN Row1");
-        form.setField(" DE PARTICIPACIÓN Row2", " DE PARTICIPACIÓN Row2");
-        form.setField(" DE PARTICIPACIÓN Row3", " DE PARTICIPACIÓN Row3");
-        form.setField(" DE PARTICIPACIÓN Row4", " DE PARTICIPACIÓN Row4");
-        form.setField("Text9", "9");
-        form.setField("Text8", "8");
-        form.setField("Text7", "7");
-        form.setField("enfermedad que ha padecido Tipo de enfermedad", "enfermedad que ha padecido Tipo de enfermedad");
-        form.setField("Text6", "6");
-        form.setField("No de Identificación", "No de Identificación");
-        form.setField("Text5", "5");
-        form.setField("Text4", "4");
-        form.setField("Text3", "3");
-        form.setField("Nombres y Apellidos DATOS DEL ASEGURADO", "Nombres y Apellidos DATOS DEL ASEGURADO");
-        form.setField("Text1", "1");
-        form.setField("NOMBRESRow1", "NOMBRESRow1");
-        form.setField("NOMBRESRow3", "NOMBRESRow3");
-        form.setField("NOMBRESRow2", "NOMBRESRow2");
-        form.setField("NOMBRESRow4", "NOMBRESRow4");
-        form.setField("Código de la Oficina", "Código de la Oficina");
-        form.setField("Dirección de Residencia", "Dirección de Residencia");
-        form.setField("País de Residencia", "País de Residencia");
-        form.setField("Departamento", "Departamento");
-        form.setField("Nombre del Comercial", "Nombre del Comercial");
-        form.setField("Cédula del Comercial", "Cédula del Comercial");
-        form.setField("Valor Crédito con Seguro", "Valor Crédito con Seguro");
-        form.setField("No de Documento", "No de Documento");
-
+        form.setField( "BIRTH_DAY2" , "5");
+        form.setField( "NATIONALITY" , "Colombiano");
+        form.setField( "NAMES" , "Jorge Andres Atuesta Maestre");
+        form.setField( "BIRTH_DAY1" , "0");
+        form.setField( "DEPARTAMENT" , "Bogota");
+        form.setField( "INITIAL_CREDIT_AMOUNT" , "2500000");
+        form.setField( "PHONE" , "5647783");
+        form.setField( "RESIDENCE_COUNTRY" , "Colombia");
+        form.setField( "SOL_YEAR" , "2019");
+        form.setField( "BIRTH_MONTH2" , "8");
+        form.setField( "MAIL" , "jatuest@bancodebogota.com.co");
+        form.setField( "SOL_MONTH" , "07");
+        form.setField( "BIRTH_MONTH1" , "0");
+        form.setField( "CONSECUTIVE_NUMBER" , "0000001");
+        form.setField( "OFFICE_NAME" , "OFICINA PRINCIPAL");
+        form.setField( "IDENTIFICATION_NUMBER_SIGN" , "1032393128");
+        form.setField( "SOL_DAY" , "23");
+        form.setField( "SELLER_IDENTIFICATION" , "1098468987");
+        form.setField( "CREDIT_AND_INSURANCE_AMOUNT" , "3000000");
+        form.setField( "BIRTH_YEAR" , "1987");
+        form.setField( "RESIDENCE_ADDRESS" , "Carrera 36 # 17-333 Torre 1 Apartamento 504");
+        form.setField( "BIRTHPLACE" , "Bogota");
+        form.setField( "INSURANCE_AMOUNT" , "500000");
+        form.setField( "CITY" , "Bogota");
+        form.setField( "SELLER_NAME" , "Npmbre del vendedor");
+        form.setField( "IDENTIFICATION_NUMBER" , "1032393128");
+        form.setField( "OFFICE_CODE" , "00000");
         for (String fldName : fldNames) {
-            System.out.println("form.setField(\"" + fldName + "\", \"" + fldName + "\");");
+            System.out.println("public static final String " + fldName + " = \"" + fldName +"\";");
         }
 
 
-        /*String states[] = form.getAppearanceStates("Check Box16");
-        form.setField("Check Box11", states[0]);
-        form.setField("Check Box13", states[0]);
-        form.setField("Check Box12", states[0]);
-        form.setField("Check Box15", states[0]);
-        form.setField("Check Box14", states[0]);
-        form.setField("Check Box17", states[0]);
-        form.setField("Check Box16", states[0]);
-        form.setField("Check Box18", states[0]);*/
+        String states[] = form.getAppearanceStates("MALE");
+        form.setField( "IDENTIFICATION_CE" , states[0]);
+        form.setField( "IDENTIFICATION_PS" , states[0]);
+        form.setField( "IDENTIFICATION_CC" , states[0]);
+        form.setField( "FEMALE" , states[0]);
+        form.setField( "MALE" , states[0]);
+        form.setField( "GNV_SALE" , states[0]);
+        form.setField( "UNIT_SALE" , states[0]);
+        form.setField( "OFFICE_SALE" , states[0]);
 
 
         stamper.setFormFlattening(true);
